418764344fffd360777ea762ecde077545b94b9b
Starlight3 <prakhar.d9@gmail.com>
2017-05-06 05:29:55 +0530

    Update imap.py
    Added email body parsing.

--- imap.py
+++ imap.py
@@ -1,14 +1,26 @@
+
 import sys
 import imaplib
+import getpass
 import email
 import email.header
+import datetime
+from email.message import Message
 
 EMAIL_ACCOUNT = "subscribe.ai.client@gmail.com"
 
 EMAIL_FOLDER = "Inbox"
 
+def getBody(email_message_instance):
+    maintype = email_message_instance.get_content_maintype()
+    if maintype == 'multipart':
+        for part in email_message_instance.get_payload():
+            if part.get_content_maintype() == 'text':
+                return part.get_payload()
+    elif maintype == 'text':
+        return email_message_instance.get_payload()
 
-def process_mailbox(M):
+def mailReader(M):
     rv, data = M.search(None, "ALL")
     if rv != 'OK':
         print("No messages found!")
@@ -22,28 +34,34 @@ def process_mailbox(M):
 
     msg = email.message_from_bytes(data[0][1])
     hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
-    subject = str(hdr)
-    return (subject)
+    #subject = str(hdr)
+    rawEmail = data[0][1]
+    email_message = email.message_from_bytes(rawEmail)
+    mail = getBody(email_message)
+    fromAddrTotal = email.utils.parseaddr(email_message['From'])
+    fromAddr = fromAddrTotal[1]
+    argument = [fromAddr , mail]
+    return argument
+    
+def auth():
+    M = imaplib.IMAP4_SSL('imap.gmail.com')
+    try:
+        rv, data = M.login(EMAIL_ACCOUNT, 'Algoproject')
+    except imaplib.IMAP4.error:
+        print ("LOGIN FAILED!!! ")
+        sys.exit(1)
 
+    print(rv, data)
 
-M = imaplib.IMAP4_SSL('imap.gmail.com')
+    rv, mailboxes = M.list()
 
-try:
-    rv, data = M.login(EMAIL_ACCOUNT, 'Algoproject')
-except imaplib.IMAP4.error:
-    print ("LOGIN FAILED!!! ")
-    sys.exit(1)
-
-# print(rv, data)
-
-rv, mailboxes = M.list()
-
-rv, data = M.select(EMAIL_FOLDER)
-if rv == 'OK':
-    # print("Processing mailbox...\n")
-    process_mailbox(M)
-    M.close()
-else:
-    print("ERROR: Unable to open mailbox ", rv)
-
-M.logout()
+    rv, data = M.select(EMAIL_FOLDER)
+    mailstuff=[]
+    if rv == 'OK':
+        mailStuff=mailReader(M)
+        M.close()
+    else:
+        print("ERROR: Unable to open mailbox ", rv)
+    M.logout()
+    return mailstuff
+auth()
