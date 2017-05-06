fb4435f1d6a6fca2776d3784696d9e89465ec323
Starlight3 <prakhar.d9@gmail.com>
2017-05-06 08:46:41 +0530

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -1,15 +1,20 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+from imap import auth
+from rake import rake
+from crawler import calling
+name = rake()
+msg = func_calling(name)
+toID = auth()[0]
 fromaddr = "subscribe.ai.client@gmail.com"
-toaddr = "prakhar.d9@gmail.com"
+toaddr = toID
 msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = toaddr
-msg['Subject'] = "PLEASE REPLY"
+msg['Subject'] = "Response to your query"
 
-body = "This is a smtp server generated mail. Please reply to confirm it has been received"
+body = name
 msg.attach(MIMEText(body, 'plain'))
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
