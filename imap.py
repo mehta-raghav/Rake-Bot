8e3cc98fbbdd2280950573f2ab9f372f0952d16b
Raghav <raghav.mehta.17@gmail.com>
2017-05-06 08:04:46 +0530

    Initial Commit

--- imap.py
+++ imap.py
@@ -1,4 +1,3 @@
-
 import sys
 import imaplib
 import getpass
@@ -11,6 +10,7 @@ EMAIL_ACCOUNT = "subscribe.ai.client@gmail.com"
 
 EMAIL_FOLDER = "Inbox"
 
+
 def getBody(email_message_instance):
     maintype = email_message_instance.get_content_maintype()
     if maintype == 'multipart':
@@ -19,6 +19,7 @@ def getBody(email_message_instance):
                 return part.get_payload()
     elif maintype == 'text':
         return email_message_instance.get_payload()
+
 
 def mailReader(M):
     rv, data = M.search(None, "ALL")
@@ -33,16 +34,17 @@ def mailReader(M):
             return
 
     msg = email.message_from_bytes(data[0][1])
-    #hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
-    #subject = str(hdr)
+    # hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
+    # subject = str(hdr)
     rawEmail = data[0][1]
     email_message = email.message_from_bytes(rawEmail)
     mail = getBody(email_message)
     fromAddrTotal = email.utils.parseaddr(email_message['From'])
     fromAddr = fromAddrTotal[1]
-    argument = [fromAddr , mail]
+    argument = [fromAddr, mail]
     return argument
-    
+
+
 def auth():
     M = imaplib.IMAP4_SSL('imap.gmail.com')
     try:
@@ -56,12 +58,14 @@ def auth():
     rv, mailboxes = M.list()
 
     rv, data = M.select(EMAIL_FOLDER)
-    mailstuff=[]
+    mailstuff = []
     if rv == 'OK':
-        mailStuff=mailReader(M)
+        mailStuff = mailReader(M)
         M.close()
     else:
         print("ERROR: Unable to open mailbox ", rv)
     M.logout()
     return mailstuff
+
+
 auth()
